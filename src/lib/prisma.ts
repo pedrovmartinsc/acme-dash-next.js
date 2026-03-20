06ef453dee5641f762dccf82bfb263dfe62bbfd6
Pedro Victor Martins Câmara <pedrovmartinscamara@gmail.com>
2026-03-20 20:37:38 +0000

    chore: consertando palavra no prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unsknown as {
+const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined;
 };
 
