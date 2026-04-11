ad6a47079dd61a2d0f3b15cd9d9ad09af140b640
Pedro Victor Martins Câmara <pedrovmartinscamara@gmail.com>
2026-04-11 12:48:33 +0000

    feat: gerar populacao no banco de dados

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,21 @@
-import { PrismaClient,InvoiceStatus} from '@prisma/client';
+import { PrismaClient, InvoiceStatus } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 import { Acme } from 'next/font/google';
 
 const prisma = new PrismaClient();
 
-async function main(){
+async function main() {
   console.log('Iniciado população do banco de dados...');
 
   const password = await bcrypt.hash('password', 10);
-  
+
   const user = await prisma.user.upsert({
-    where: { email: 'admin@acme.com'},
+    where: { email: 'admin@acme.com' },
     update: {},
     create: {
-        name: 'Admin',
-        email: 'admin@acme.com',
-        password: password
+      name: 'Admin',
+      email: 'admin@acme.com',
+      password: password
     }
   });
 
@@ -25,12 +25,12 @@ async function main(){
     name: 'Alex Bessa',
     email: 'alex@email.com',
     imageUrl: 'https://ui-avatars.com/api/?nome=Alex+Bessa&background=random'
-  },{
+  }, {
     name: 'Valdiana Bessa',
     email: 'valdiana@email.com',
     imageUrl: 'https://ui-avatars.com/api/?nome=Valdiana+Bessa&background=random'
 
-}
+  }
 {
     name: 'Timóteo Bessa',
     email: 'timoteo@email.com',
@@ -40,88 +40,125 @@ async function main(){
 
   const customers = [];
 
-  for (const data of customer_data){
+  for (const data of customer_data) {
     const customer = await prisma.customer.upsert({
-        where: {email: data.email},
-        update: {},
-        create: data
+      where: { email: data.email },
+      update: {},
+      create: data
     });
 
     customers.push(customer);
-    console.log(`Cliente criado: ${ customer.name }`);
+    console.log(`Cliente criado: ${customer.name}`);
 
 
   };
   const invoiceData = [
-  { 
-    amount: 15785, 
-    status: InvoiceStatus.PENDENTE, 
-    data: '2026-01-12', 
-    customer: customers[0] 
-  },
-  { 
-    amount: 15722285, 
-    status: InvoiceStatus.PENDENTE, 
-    data: '2026-02-15', 
-    customer: customers[1] 
-  },
-  { 
-    amount: 85400, 
-    status: InvoiceStatus.PAGO, 
-    data: '2026-03-10', 
-    customer: customers[2] 
-  },
-  { 
-    amount: 15733385, 
-    status: InvoiceStatus.PENDENTE, 
-    data: '2026-04-22', 
-    customer: customers[0] 
-  },
-  { 
-    amount: 15781212125, 
-    status: InvoiceStatus.PAGO, 
-    data: '2026-05-05', 
-    customer: customers[1] 
-  },
-  { 
-    amount: 450000, 
-    status: InvoiceStatus.PENDENTE, 
-    data: '2026-06-18', 
-    customer: customers[2] 
-  },
-  { 
-    amount: 15732424285, 
-    status: InvoiceStatus.PENDENTE, 
-    data: '2026-07-20', 
-    customer: customers[0] 
-  },
-  { 
-    amount: 157241285, 
-    status: InvoiceStatus.PAGO, 
-    data: '2026-08-30', 
-    customer: customers[1] 
-  },
-  { 
-    amount: 987654321, 
-    status: InvoiceStatus.PAGO, 
-    data: '2026-09-14', 
-    customer: customers[2] 
-  },
-  { 
-    amount: 15785432324, 
-    status: InvoiceStatus.PENDENTE, 
-    data: '2026-10-11', 
-    customer: customers[0] 
-  }
-];
-
-
-for (const data of invoiceData){
-  await prisma.invoice.create({
-    data:{
-      amount: invoice.amount,
-      status:, invoice
+    {
+      amount: 15785,
+      status: InvoiceStatus.PENDENTE,
+      date: '2026-01-12',
+      customer: customers[0]
+    },
+    {
+      amount: 15722285,
+      status: InvoiceStatus.PENDENTE,
+      date: '2026-02-15',
+      customer: customers[1]
+    },
+    {
+      amount: 85400,
+      status: InvoiceStatus.PAGO,
+      date: '2026-03-10',
+      customer: customers[2]
+    },
+    {
+      amount: 15733385,
+      status: InvoiceStatus.PENDENTE,
+      date: '2026-04-22',
+      customer: customers[0]
+    },
+    {
+      amount: 15781212125,
+      status: InvoiceStatus.PAGO,
+      date: '2026-05-05',
+      customer: customers[1]
+    },
+    {
+      amount: 450000,
+      status: InvoiceStatus.PENDENTE,
+      date: '2026-06-18',
+      customer: customers[2]
+    },
+    {
+      amount: 15732424285,
+      status: InvoiceStatus.PENDENTE,
+      date: '2026-07-20',
+      customer: customers[0]
+    },
+    {
+      amount: 157241285,
+      status: InvoiceStatus.PAGO,
+      date: '2026-08-30',
+      customer: customers[1]
+    },
+    {
+      amount: 987654321,
+      status: InvoiceStatus.PAGO,
+      date: '2026-09-14',
+      customer: customers[2]
+    },
+    {
+      amount: 15785432324,
+      status: InvoiceStatus.PENDENTE,
+      date: '2026-10-11',
+      customer: customers[0]
     }
-  })
-}
+  ];
+
+
+  for (const data of invoiceData) {
+    await prisma.invoice.create({
+      data: {
+        amount: data.amount,
+        status: data.status,
+        date: new Date(data.date),
+        customerId: data.customer.id
+      }
+    });
+  };
+  console.log(`${invoiceData.length} fatura criadas.`);
+
+  const revenueData = [
+    { month: 'Jan', revenue: 65748461 },
+    { month: 'Fen', revenue: 69562131 },
+    { month: 'Mar', revenue: 8556565 },
+    { month: 'Abr', revenue: 95653 },
+    { month: 'Mai', revenue: 9756232 },
+    { month: 'Jun', revenue: 98465103 },
+    { month: 'Jul', revenue: 1541656 },
+    { month: 'Ago', revenue: 8979613 },
+    { month: 'Set', revenue: 784152103 },
+    { month: 'Out', revenue: 3265232 },
+    { month: 'Nov', revenue: 1656566565 },
+    { month: 'Dez', revenue: 646562266 },
+  ];
+
+  for (const data of revenueData){
+    await prisma.revenue.upsert({
+      where: { month: data.month },
+      update: { revunue: data.revenue },
+      create: data
+    });
+  };
+  console.log('Dados de receita mensal criados.');
+
+  console.log('População concluída com sucess.');
+  
+  main()
+   .catch(() => {
+    console.log('Erro ao popular o banco:', Error);
+   })
+   .finally( async() => {
+     await prisma.$disconnect();
+   })
 };
